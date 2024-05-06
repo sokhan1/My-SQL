7d2586df8dd5ecfe7ab49b127b5e8258c791e4ba
SEOKHWAN KIM <99132328+sokhan1@users.noreply.github.com>
2024-05-06 19:31:37 +0900

    Create Web Application.sql
    Web Application.sql

---
+++ Web Application.sql
@@ -0,0 +1,15 @@
+CREATE TABLE nodejs.comments(
+id INT NOT NULL AUTO_INCREMENT,
+commenter INT NOT NULL,
+comment VARCHAR(100) NOT NULL,
+created_at DATETIME NOT NULL DEFAULT now(),
+PRIMARY KEY(id),
+INDEX commenter_idx (commenter ASC),
+CONSTRAINT commenter
+FOREIGN KEY (commenter)
+REFERENCES nodejs.users (id)
+ON DELETE CASCADE
+ON UPDATE CASCADE)
+COMMENT = 'reply'
+DEFAULT CHARSET=utf8mb4
+ENGINE=InnoDB;
